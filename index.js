697896635a7d25e4f92b714e63cae69236f21fb0
Geza Kovacs <gkovacs@stanford.edu>
2014-12-03 04:06:54 -0800

    updated vocab lists

--- index.js
+++ index.js
@@ -1,47 +1,84 @@
 (function(){
-  var root, J, findIndex, japanese1_vocabulary_text, chinese1_vocabulary_text, korean1_vocabulary_text, vietnamese1_vocabulary_text, parseVocab, flashcard_sets, language_names, flashcard_name_aliases, firstNonNull, setFlashcardSet, selectIdx, selectElem, selectNElem, selectNElemExceptElem, swapIdxInList, shuffleList, deepCopy, newQuestion, playSound, playSoundCurrentWord, questionWithWords, getUrlParameters, gotoNewQuizPage, gotoQuizPage, gotoOptionPage, gotoChatPage, changeLang, gotoPage, slice$ = [].slice, out$ = typeof exports != 'undefined' && exports || this;
+  var root, J, findIndex, parseVocab, japanese_all_vocabulary_text, chinese1_vocabulary_text, korean1_vocabulary_text, vietnamese1_vocabulary_text, japanese_vocab_all, flashcard_sets, language_names, flashcard_name_aliases, firstNonNull, setFlashcardSet, selectIdx, selectElem, selectNElem, selectNElemExceptElem, swapIdxInList, shuffleList, deepCopy, newQuestion, playSound, playSoundCurrentWord, questionWithWords, getUrlParameters, gotoQuizPage, gotoOptionPage, gotoChatPage, changeLang, setInsertionFormat, changeFeedInsertionFormat, setFullName, changeFullName, gotoPage, slice$ = [].slice, out$ = typeof exports != 'undefined' && exports || this;
   root = typeof exports != 'undefined' && exports !== null ? exports : this;
   J = $.jade;
   findIndex = require('prelude-ls').findIndex;
-  japanese1_vocabulary_text = 'dog | inu\ncat | neko\nhorse | uma\nmouse | nezumi\nbird | tori\ncow | ushi\nfish | sakana\nman | otoko\nwoman | onna\nred | akai\nblue | aoi\ngreen | midori\nyellow | kiiro\npurple | murasaki\nuniversity | daigaku\nstudent | gakusei\nmorning | asa\nnight | yoru\nyesterday | kinō\ntoday | kyō\ntomorrow | ashita\nspring | haru\nsummer | natsu\nautumn | aki\nwinter | fuyu';
-  chinese1_vocabulary_text = 'dog | gǒu\ncat | māo\nhorse | mǎ\nmouse | lǎoshǔ\nbird | niǎo\ncow | niú\nfish | yú\nman | nánrén\nwoman | nǚrén\nred | hóng sè\nblue | lán sè\ngreen | lǜ sè\nyellow | huáng sè\npurple | zǐ sè\nuniversity | dàxué\nstudent | xuéshēng\nmorning | zǎoshàng\nnight | yèwǎn\nyesterday | zuótiān\ntoday | jīntiān\ntomorrow | míngtiān\nspring | chūntiān\nsummer | xiàtiān\nautumn | qiūtiān\nwinter | dōngtiān';
-  korean1_vocabulary_text = 'dog | gae\ncat | goyang-i\nhorse | mal\nmouse | jwi\nbird | sae\ncow | so\nfish | mulgogi\nman | namja\nwoman | yeoja\nred | ppalgansaeg\nblue | puleunsaeg\ngreen | nogsaeg\nyellow | hwangsaeg\npurple | bolasaeg\nuniversity | daehag\nstudent | hagsaeng\nmorning | achim\nnight | bam\nyesterday | eoje\ntoday | oneul\ntomorrow | naeil\nspring | bom\nsummer | yeoleum\nautumn | ga-eul\nwinter | gyeoul';
-  vietnamese1_vocabulary_text = 'dog | chó\ncat | con mèo\nhorse | ngựa\nmouse | chuột\nbird | chim\ncow | bò\nfish | cá\nman | đàn ông\nwoman | đàn bà\nred | đỏ\nblue | xanh lam\ngreen | xanh lá cây\nyellow | vàng\npurple | màu đỏ tía\nuniversity | trường đại học\nstudent | sinh viên\nmorning | buổi sáng\nnight | đêm\nyesterday | hôm qua\ntoday | ngày nay\ntomorrow | ngày mai\nspring | mùa xuân\nsummer | mùa hè\nautumn | mùa thu\nwinter | mùa đông';
-  parseVocab = function(vocab_text){
-    var output, i$, ref$, len$, idx, line, ref1$, english, romaji;
+  parseVocab = function(){
+    var vocab_text_list, output, i$, len$, vocab_text, j$, ref$, len1$, idx, line, ref1$, english, romaji;
+    vocab_text_list = slice$.call(arguments);
     output = [];
-    for (i$ = 0, len$ = (ref$ = vocab_text.split('\n')).length; i$ < len$; ++i$) {
-      idx = i$;
-      line = ref$[i$];
-      line = line.trim();
-      if (line.length > 0) {
-        ref1$ = line.split('|'), english = ref1$[0], romaji = ref1$[1];
-        english = english.trim();
-        romaji = romaji.trim();
-        output.push({
-          english: english,
-          romaji: romaji,
-          idx: idx
-        });
+    for (i$ = 0, len$ = vocab_text_list.length; i$ < len$; ++i$) {
+      vocab_text = vocab_text_list[i$];
+      for (j$ = 0, len1$ = (ref$ = vocab_text.split('\n')).length; j$ < len1$; ++j$) {
+        idx = j$;
+        line = ref$[j$];
+        line = line.trim();
+        if (line.length > 0 && line[0] !== '#') {
+          ref1$ = line.split('|'), english = ref1$[0], romaji = ref1$[1];
+          english = english.trim();
+          romaji = romaji.trim();
+          output.push({
+            english: english,
+            romaji: romaji,
+            idx: idx
+          });
+        }
       }
     }
     return output;
   };
+  /*
+  japanese1_vocabulary_text = '''
+  dog | inu
+  cat | neko
+  horse | uma
+  mouse | nezumi
+  bird | tori
+  cow | ushi
+  fish | sakana
+  man | otoko
+  woman | onna
+  red | akai
+  blue | aoi
+  green | midori
+  yellow | kiiro
+  purple | murasaki
+  university | daigaku
+  student | gakusei
+  morning | asa
+  night | yoru
+  yesterday | kinō
+  today | kyō
+  tomorrow | ashita
+  spring | haru
+  summer | natsu
+  autumn | aki
+  winter | fuyu
+  '''
+  */
+  japanese_all_vocabulary_text = '# people\n\nperson | hito\nman | otoko\nwoman | onna\nchildren | kodomo\nfamily | kazoku\n# I, myself | watashi\n# girl | shōjo\n# boy | shōnen\n\n# occupations\n\ndoctor | isha\nnurse | kangoshi\ndentist | shikai\npolitician | seijika\nlawyer | bengoshi\nfirefighter | shōbōshi\nsoldier | heishi\narchitect | kenchikuka\nteacher | sensei\nsinger | kashu\n\n# body\n\nfoot | ashita\nhead | atama\nface | kao\nmouth | kuchi\nlips | kuchibiru\nhair | kami\near | mimi\narm | ude\nfinger | yubi\nneck | kubi\nheart | shinzō\nchest | mune\nillness | byōki\n\n# family\n\nparents | oya\nfather | chichi\nmother | haha\nwife | tsuma\nhusband | otto\n# older brother | ani\n# older sister | ane\nyounger brother | otōto\nyounger sister | imōto\n# siblings | kyōdai\ngrandfather | sofu\ngrandmother | sobo\ngrandchild | mago\nuncle | oji\naunt | oba\n\n# life\n\n# living creatures | ikimono\nmonster | bakemono\n\n# animals\n\nanimal | dōbutsu\ndog | inu\ncat | neko\ncow | ushi\npig | buta\nhorse | uma\nsheep | hitsuji\nmonkey | saru\nmouse | nezumi\ntiger | tora\nwolf | ōkami\nrabbit | usagi\ndragon | ryū\ndeer | shika\nfrog | kaeru\ntoad | gama\nlion | shishi\ngiraffe | kirin\nelephant | zō\nbird | tori\nchicken | niwatori\nsparrow | suzume\ncrow | karasu\neagle | washi\nhawk, falcon | taka\nfish | sakana\nshrimp | ebi\ntuna | maguro\noctopus | tako\ninsect | mushi\nbutterfly | chō\nspider | kumo\nfirefly | hotaru\ncockroach | gokiburi\nearthworm | mimizu\nlizard | tokage\nsnake | hebi\n\n# plants\n\nplants | shokubutsu\ngrass | kusa\nflower | hana\ntree | ki\nmushroom | kinoko\n\n# crops\n\nuncooked rice | kome\nwheat, oats | mugi\nvegetable | yasai\nyam, potato | imo\nbeans | mame\nfruit | kudamono\ncarrot | ninjin\napple | ringo\norange | mikan\npear | nashi\nchestnut | kuri\npeach | momo\ntomato | banka\nwatermelon | suika\n\n# food\n\nbreakfast | asagohan\nlunch | hirugohan\nsupper | bangohan\ncooked rice, food | gohan\ncooking | ryōri\n\n# drink\n\nbeverage | nomimono\nmilk | gyūnyū\n\n# seasoning\n\nsugar | satō\nsalt | shio\nsoy sauce | shōyu\n\n# time and space\n\ntime | jikan\nday | nichi\nmonth | gatsu\nyear | toshi\n# last year | kyōnen\n# this year | kotoshi\n# next year | rainen\ntoday | kyō\nyesterday | kinō\ntomorrow | ashita\nmorning | asa\nnight | yoru\nmoon | tsuki\nstar | hoshi\nweather | tenki\nrain | ame\ncloudy | kumori\nsnow | yuki\nwind | kaze\nthunder | kaminari\nstorm | arashi\nsky | sora\n\n# directions and positions\n\neast | higashi\nwest | nishi\nsouth | minami\nnorth | kita\n# here | koko\n# there | soko\n# over there | asoko\nright (direction) | migi\nleft | hidari\nabove, up | ue\nbelow, down | shita\nin front of | mae\nbehind | ushiro\n\n# materials\n\nwater | mizu\nice | kōri\nearth, ground | tsuchi\n\n# society\n\nsociety | shakai\neconomy | keizai\ncompany | kaisha\nmeeting | kaigi\nschool | gakkō\nstore | mise\nfactory | kōjō\nmoney | okane\nticket | kippu\n\n# man made objects\n\n# home\n\ndesk | tsukue\nwindow | mado\nelectricity | denki\n\n# tools\n\n# stationery\n\ndiary | nikki\npencil | enpitsu\neraser | keshigomu\nscissors | hasami\n\n# clothes\n\nclothes | fuku\nshoes | kutsu\nglasses | megane\n\n# transport \n\nairplane | hikōki\nstreet | michi\ntrain | densha\ncar | jidōsha\n';
+  chinese1_vocabulary_text = 'dog | gǒu\ncat | māo\nhorse | mǎ\nmouse | lǎoshǔ\nbird | niǎo\ncow | niú\nfish | yú\nman | nánrén\nwoman | nǚrén\nred | hóng sè\nblue | lán sè\ngreen | lǜ sè\nyellow | huáng sè\npurple | zǐ sè\nuniversity | dàxué\nstudent | xuéshēng\nmorning | zǎoshàng\nnight | yèwǎn\nyesterday | zuótiān\ntoday | jīntiān\ntomorrow | míngtiān\nspring | chūntiān\nsummer | xiàtiān\nautumn | qiūtiān\nwinter | dōngtiān';
+  korean1_vocabulary_text = 'dog | gae\ncat | goyang-i\nhorse | mal\nmouse | jwi\nbird | sae\ncow | so\nfish | mulgogi\nman | namja\nwoman | yeoja\nred | ppalgansaeg\nblue | puleunsaeg\ngreen | nogsaeg\nyellow | hwangsaeg\npurple | bolasaeg\nuniversity | daehag\nstudent | hagsaeng\nmorning | achim\nnight | bam\nyesterday | eoje\ntoday | oneul\ntomorrow | naeil\nspring | bom\nsummer | yeoleum\nautumn | ga-eul\nwinter | gyeoul';
+  vietnamese1_vocabulary_text = 'dog | chó\ncat | con mèo\nhorse | ngựa\nmouse | chuột\nbird | chim\ncow | bò\nfish | cá\nman | đàn ông\nwoman | đàn bà\nred | đỏ\nblue | xanh lam\ngreen | xanh lá cây\nyellow | vàng\npurple | màu đỏ tía\nuniversity | trường đại học\nstudent | sinh viên\nmorning | buổi sáng\nnight | đêm\nyesterday | hôm qua\ntoday | ngày nay\ntomorrow | ngày mai\nspring | mùa xuân\nsummer | mùa hè\nautumn | mùa thu\nwinter | mùa đông';
+  japanese_vocab_all = parseVocab(japanese_all_vocabulary_text);
   flashcard_sets = {
-    'japanese1': parseVocab(japanese1_vocabulary_text),
+    'japanese1': [japanese_vocab_all[0], japanese_vocab_all[1], japanese_vocab_all[2], japanese_vocab_all[3], japanese_vocab_all[4], japanese_vocab_all[5], japanese_vocab_all[6], japanese_vocab_all[7], japanese_vocab_all[8], japanese_vocab_all[9], japanese_vocab_all[10], japanese_vocab_all[11], japanese_vocab_all[12], japanese_vocab_all[13], japanese_vocab_all[14], japanese_vocab_all[15], japanese_vocab_all[16], japanese_vocab_all[17], japanese_vocab_all[18], japanese_vocab_all[19], japanese_vocab_all[20], japanese_vocab_all[21], japanese_vocab_all[22], japanese_vocab_all[23], japanese_vocab_all[24], japanese_vocab_all[25], japanese_vocab_all[26], japanese_vocab_all[27], japanese_vocab_all[28], japanese_vocab_all[29], japanese_vocab_all[30], japanese_vocab_all[31], japanese_vocab_all[32], japanese_vocab_all[33], japanese_vocab_all[34], japanese_vocab_all[35], japanese_vocab_all[36], japanese_vocab_all[37], japanese_vocab_all[38], japanese_vocab_all[39], japanese_vocab_all[40], japanese_vocab_all[41], japanese_vocab_all[42], japanese_vocab_all[43], japanese_vocab_all[44], japanese_vocab_all[45], japanese_vocab_all[46], japanese_vocab_all[47], japanese_vocab_all[48], japanese_vocab_all[49]],
+    'japanese2': [japanese_vocab_all[50], japanese_vocab_all[51], japanese_vocab_all[52], japanese_vocab_all[53], japanese_vocab_all[54], japanese_vocab_all[55], japanese_vocab_all[56], japanese_vocab_all[57], japanese_vocab_all[58], japanese_vocab_all[59], japanese_vocab_all[60], japanese_vocab_all[61], japanese_vocab_all[62], japanese_vocab_all[63], japanese_vocab_all[64], japanese_vocab_all[65], japanese_vocab_all[66], japanese_vocab_all[67], japanese_vocab_all[68], japanese_vocab_all[69], japanese_vocab_all[70], japanese_vocab_all[71], japanese_vocab_all[72], japanese_vocab_all[73], japanese_vocab_all[74], japanese_vocab_all[75], japanese_vocab_all[76], japanese_vocab_all[77], japanese_vocab_all[78], japanese_vocab_all[79], japanese_vocab_all[80], japanese_vocab_all[81], japanese_vocab_all[82], japanese_vocab_all[83], japanese_vocab_all[84], japanese_vocab_all[85], japanese_vocab_all[86], japanese_vocab_all[87], japanese_vocab_all[88], japanese_vocab_all[89], japanese_vocab_all[90], japanese_vocab_all[91], japanese_vocab_all[92], japanese_vocab_all[93], japanese_vocab_all[94], japanese_vocab_all[95], japanese_vocab_all[96], japanese_vocab_all[97], japanese_vocab_all[98], japanese_vocab_all[99]],
+    'japanese3': [japanese_vocab_all[100], japanese_vocab_all[101], japanese_vocab_all[102], japanese_vocab_all[103], japanese_vocab_all[104], japanese_vocab_all[105], japanese_vocab_all[106], japanese_vocab_all[107], japanese_vocab_all[108], japanese_vocab_all[109], japanese_vocab_all[110], japanese_vocab_all[111], japanese_vocab_all[112], japanese_vocab_all[113], japanese_vocab_all[114], japanese_vocab_all[115], japanese_vocab_all[116], japanese_vocab_all[117], japanese_vocab_all[118], japanese_vocab_all[119], japanese_vocab_all[120], japanese_vocab_all[121], japanese_vocab_all[122], japanese_vocab_all[123], japanese_vocab_all[124], japanese_vocab_all[125], japanese_vocab_all[126], japanese_vocab_all[127], japanese_vocab_all[128], japanese_vocab_all[129], japanese_vocab_all[130], japanese_vocab_all[131], japanese_vocab_all[132], japanese_vocab_all[133], japanese_vocab_all[134], japanese_vocab_all[135], japanese_vocab_all[136], japanese_vocab_all[137], japanese_vocab_all[138], japanese_vocab_all[139], japanese_vocab_all[140], japanese_vocab_all[141], japanese_vocab_all[142], japanese_vocab_all[143], japanese_vocab_all[144], japanese_vocab_all[145], japanese_vocab_all[146], japanese_vocab_all[147], japanese_vocab_all[148], japanese_vocab_all[149]],
     'chinese1': parseVocab(chinese1_vocabulary_text),
     'korean1': parseVocab(korean1_vocabulary_text),
     'vietnamese1': parseVocab(vietnamese1_vocabulary_text)
   };
   language_names = {
     'japanese1': 'Japanese',
+    'japanese2': 'Japanese',
+    'japanese3': 'Japanese',
     'chinese1': 'Chinese',
     'korean1': 'Korean',
     'vietnamese1': 'Vietnamese'
   };
   flashcard_name_aliases = {
-    'ja': 'japanese1',
-    'japanese': 'japanese1',
+    'ja1': 'japanese1',
     'zh': 'chinese1',
     'chn': 'chinese1',
     'chinese': 'chinese1',
@@ -246,33 +283,50 @@
     });
     return map;
   };
-  out$.gotoNewQuizPage = gotoNewQuizPage = function(){
-    var param;
-    $('.mainpage').hide();
-    $('#quizpage').show();
-    param = getUrlParameters();
-    setFlashcardSet(firstNonNull(param.lang, param.language, param.quiz, param.lesson, param.flashcard, param.flashcardset, $.cookie('lang'), 'vietnamese1'));
-    newQuestion();
-  };
   out$.gotoQuizPage = gotoQuizPage = function(){
     $('.mainpage').hide();
     $('#quizpage').show();
+    if (root.currentWord == null) {
+      newQuestion();
+    }
   };
   out$.gotoOptionPage = gotoOptionPage = function(){
     $('.mainpage').hide();
     $('#optionpage').show();
-    $('#langselect').val(root.current_language_name);
+    $('#langselect').val(root.current_flashcard_set);
+    $('#formatselect').val($.cookie('format'));
+    $('#fullnameinput').val($.cookie('fullname'));
   };
   out$.gotoChatPage = gotoChatPage = function(){
     $('.mainpage').hide();
     $('#chatpage').show();
-    $('#currentanswer').text(currentWord.romaji + ' = ' + currentWord.english);
+    $('#currentanswer').text(root.currentWord.romaji + ' = ' + root.currentWord.english);
   };
   out$.changeLang = changeLang = function(){
     var newlang;
     newlang = $('#langselect').val();
     setFlashcardSet(newlang);
     newQuestion();
+  };
+  out$.setInsertionFormat = setInsertionFormat = function(newformat){
+    if (newformat === 'interactive' || newformat === 'link') {
+      $.cookie('format', newformat);
+    }
+  };
+  out$.changeFeedInsertionFormat = changeFeedInsertionFormat = function(){
+    var newformat;
+    newformat = $('#formatselect').val();
+    setInsertionFormat(newformat);
+  };
+  out$.setFullName = setFullName = function(newfullname){
+    if (newfullname != null && newfullname.length > 0) {
+      $.cookie('fullname', newfullname);
+    }
+  };
+  out$.changeFullName = changeFullName = function(){
+    var newfullname;
+    newfullname = $('#fullnameinput').val();
+    setFullName(newfullname);
   };
   gotoPage = function(page){
     switch (page) {
@@ -285,14 +339,18 @@
     }
   };
   $(document).ready(function(){
-    var param, targetpage;
-    console.log('hello world!');
+    var param, condition;
     param = getUrlParameters();
-    targetpage = firstNonNull(param.page, 'quiz');
-    if (targetpage === 'quiz') {
-      return gotoNewQuizPage();
-    } else {
-      return gotoPage(targetpage);
+    if (param.facebook != null && param.facebook !== 'false' && param.facebook !== false) {
+      condition = $.cookie('format');
+      if (condition != null && condition === 'link') {
+        window.location = '/control';
+        return;
+      }
     }
+    setFlashcardSet(firstNonNull(param.lang, param.language, param.quiz, param.lesson, param.flashcard, param.flashcardset, $.cookie('lang'), 'vietnamese1'));
+    setInsertionFormat(firstNonNull(param.format, param.condition, $.cookie('format'), 'interactive'));
+    setFullName(firstNonNull(param.fullname, $.cookie('fullname'), 'Anonymous User'));
+    return gotoPage(firstNonNull(param.page, 'quiz'));
   });
 }).call(this);
--- index.ls
+++ index.ls
@@ -4,6 +4,19 @@ J = $.jade
 
 {find-index} = require \prelude-ls
 
+parse-vocab = (...vocab_text_list) ->
+  output = []
+  for vocab_text in vocab_text_list
+    for line,idx in vocab_text.split('\n')
+      line = line.trim()
+      if line.length > 0 and line[0] != '#'
+        [english,romaji] = line.split('|')
+        english = english.trim()
+        romaji = romaji.trim()
+        output.push {english: english, romaji: romaji, idx: idx}
+  return output
+
+/*
 japanese1_vocabulary_text = '''
 dog | inu
 cat | neko
@@ -30,6 +43,248 @@ spring | haru
 summer | natsu
 autumn | aki
 winter | fuyu
+'''
+*/
+
+# taken from http://en.wiktionary.org/wiki/Appendix:1000_Japanese_basic_words
+
+japanese_all_vocabulary_text = '''
+# people
+
+person | hito
+man | otoko
+woman | onna
+children | kodomo
+family | kazoku
+# I, myself | watashi
+# girl | shōjo
+# boy | shōnen
+
+# occupations
+
+doctor | isha
+nurse | kangoshi
+dentist | shikai
+politician | seijika
+lawyer | bengoshi
+firefighter | shōbōshi
+soldier | heishi
+architect | kenchikuka
+teacher | sensei
+singer | kashu
+
+# body
+
+foot | ashita
+head | atama
+face | kao
+mouth | kuchi
+lips | kuchibiru
+hair | kami
+ear | mimi
+arm | ude
+finger | yubi
+neck | kubi
+heart | shinzō
+chest | mune
+illness | byōki
+
+# family
+
+parents | oya
+father | chichi
+mother | haha
+wife | tsuma
+husband | otto
+# older brother | ani
+# older sister | ane
+younger brother | otōto
+younger sister | imōto
+# siblings | kyōdai
+grandfather | sofu
+grandmother | sobo
+grandchild | mago
+uncle | oji
+aunt | oba
+
+# life
+
+# living creatures | ikimono
+monster | bakemono
+
+# animals
+
+animal | dōbutsu
+dog | inu
+cat | neko
+cow | ushi
+pig | buta
+horse | uma
+sheep | hitsuji
+monkey | saru
+mouse | nezumi
+tiger | tora
+wolf | ōkami
+rabbit | usagi
+dragon | ryū
+deer | shika
+frog | kaeru
+toad | gama
+lion | shishi
+giraffe | kirin
+elephant | zō
+bird | tori
+chicken | niwatori
+sparrow | suzume
+crow | karasu
+eagle | washi
+hawk, falcon | taka
+fish | sakana
+shrimp | ebi
+tuna | maguro
+octopus | tako
+insect | mushi
+butterfly | chō
+spider | kumo
+firefly | hotaru
+cockroach | gokiburi
+earthworm | mimizu
+lizard | tokage
+snake | hebi
+
+# plants
+
+plants | shokubutsu
+grass | kusa
+flower | hana
+tree | ki
+mushroom | kinoko
+
+# crops
+
+uncooked rice | kome
+wheat, oats | mugi
+vegetable | yasai
+yam, potato | imo
+beans | mame
+fruit | kudamono
+carrot | ninjin
+apple | ringo
+orange | mikan
+pear | nashi
+chestnut | kuri
+peach | momo
+tomato | banka
+watermelon | suika
+
+# food
+
+breakfast | asagohan
+lunch | hirugohan
+supper | bangohan
+cooked rice, food | gohan
+cooking | ryōri
+
+# drink
+
+beverage | nomimono
+milk | gyūnyū
+
+# seasoning
+
+sugar | satō
+salt | shio
+soy sauce | shōyu
+
+# time and space
+
+time | jikan
+day | nichi
+month | gatsu
+year | toshi
+# last year | kyōnen
+# this year | kotoshi
+# next year | rainen
+today | kyō
+yesterday | kinō
+tomorrow | ashita
+morning | asa
+night | yoru
+moon | tsuki
+star | hoshi
+weather | tenki
+rain | ame
+cloudy | kumori
+snow | yuki
+wind | kaze
+thunder | kaminari
+storm | arashi
+sky | sora
+
+# directions and positions
+
+east | higashi
+west | nishi
+south | minami
+north | kita
+# here | koko
+# there | soko
+# over there | asoko
+right (direction) | migi
+left | hidari
+above, up | ue
+below, down | shita
+in front of | mae
+behind | ushiro
+
+# materials
+
+water | mizu
+ice | kōri
+earth, ground | tsuchi
+
+# society
+
+society | shakai
+economy | keizai
+company | kaisha
+meeting | kaigi
+school | gakkō
+store | mise
+factory | kōjō
+money | okane
+ticket | kippu
+
+# man made objects
+
+# home
+
+desk | tsukue
+window | mado
+electricity | denki
+
+# tools
+
+# stationery
+
+diary | nikki
+pencil | enpitsu
+eraser | keshigomu
+scissors | hasami
+
+# clothes
+
+clothes | fuku
+shoes | kutsu
+glasses | megane
+
+# transport 
+
+airplane | hikōki
+street | michi
+train | densha
+car | jidōsha
+
 '''
 
 chinese1_vocabulary_text = '''
@@ -116,19 +371,13 @@ autumn | mùa thu
 winter | mùa đông
 '''
 
-parse-vocab = (vocab_text) ->
-  output = []
-  for line,idx in vocab_text.split('\n')
-    line = line.trim()
-    if line.length > 0
-      [english,romaji] = line.split('|')
-      english = english.trim()
-      romaji = romaji.trim()
-      output.push {english: english, romaji: romaji, idx: idx}
-  return output
+japanese_vocab_all = parse-vocab japanese_all_vocabulary_text
 
 flashcard_sets = {
-  'japanese1': parse-vocab japanese1_vocabulary_text
+  #'japanese1': parse-vocab japanese1_vocabulary_text
+  'japanese1': japanese_vocab_all[0 til 50]
+  'japanese2': japanese_vocab_all[50 til 100]
+  'japanese3': japanese_vocab_all[100 til 150]
   'chinese1': parse-vocab chinese1_vocabulary_text
   'korean1': parse-vocab korean1_vocabulary_text
   'vietnamese1': parse-vocab vietnamese1_vocabulary_text
@@ -136,14 +385,15 @@ flashcard_sets = {
 
 language_names = {
   'japanese1': 'Japanese'
+  'japanese2': 'Japanese'
+  'japanese3': 'Japanese'
   'chinese1': 'Chinese'
   'korean1': 'Korean'
   'vietnamese1': 'Vietnamese'
 }
 
 flashcard_name_aliases = {
-  'ja': 'japanese1'
-  'japanese': 'japanese1'
+  'ja1': 'japanese1'
   'zh': 'chinese1'
   'chn': 'chinese1'
   'chinese': 'chinese1'
@@ -300,35 +550,51 @@ getUrlParameters = root.getUrlParameters = ->
   )
   return map
 
-export goto-new-quiz-page = ->
-  $('.mainpage').hide()
-  $('#quizpage').show()
-  param = getUrlParameters()
-  set-flashcard-set <| first-non-null param.lang, param.language, param.quiz, param.lesson, param.flashcard, param.flashcardset, $.cookie('lang'), 'vietnamese1'
-  new-question()
-  return
-
 export goto-quiz-page = ->
   $('.mainpage').hide()
   $('#quizpage').show()
+  if not root.current-word?
+    new-question()
   return
 
 export goto-option-page = ->
   $('.mainpage').hide()
   $('#optionpage').show()
-  $('#langselect').val(root.current_language_name)
+  $('#langselect').val(root.current_flashcard_set)
+  $('#formatselect').val($.cookie('format'))
+  $('#fullnameinput').val($.cookie('fullname'))
   return
 
 export goto-chat-page = ->
   $('.mainpage').hide()
   $('#chatpage').show()
-  $('#currentanswer').text current-word.romaji + ' = ' + current-word.english
+  $('#currentanswer').text root.current-word.romaji + ' = ' + root.current-word.english
   return
 
 export change-lang = ->
   newlang = $('#langselect').val()
   set-flashcard-set newlang
   new-question()
+  return
+
+export set-insertion-format = (newformat) ->
+  if newformat == 'interactive' or newformat == 'link'
+    $.cookie('format', newformat)
+  return
+
+export change-feed-insertion-format = ->
+  newformat = $('#formatselect').val()
+  set-insertion-format newformat
+  return
+
+export set-full-name = (newfullname) ->
+  if newfullname? and newfullname.length > 0
+    $.cookie('fullname', newfullname)
+  return
+
+export change-full-name = ->
+  newfullname = $('#fullnameinput').val()
+  set-full-name newfullname
   return
 
 goto-page = (page) ->
@@ -338,10 +604,13 @@ goto-page = (page) ->
   | \chat => goto-chat-page()
 
 $(document).ready ->
-  console.log 'hello world!'
   param = getUrlParameters()
-  targetpage = first-non-null param.page, 'quiz'
-  if targetpage == 'quiz'
-    goto-new-quiz-page()
-  else
-    goto-page targetpage
+  if param.facebook? and param.facebook != 'false' and param.facebook != false
+    condition = $.cookie('format')
+    if condition? and condition == 'link'
+      window.location = '/control'
+      return
+  set-flashcard-set <| first-non-null param.lang, param.language, param.quiz, param.lesson, param.flashcard, param.flashcardset, $.cookie('lang'), 'vietnamese1'
+  set-insertion-format <| first-non-null param.format, param.condition, $.cookie('format'), 'interactive'
+  set-full-name <| first-non-null param.fullname, $.cookie('fullname'), 'Anonymous User'
+  goto-page <| first-non-null param.page, 'quiz'
