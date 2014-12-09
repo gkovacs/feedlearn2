fd8201369dd8d747528b1b181de8e242cab6a3de
Geza Kovacs <gkovacs@stanford.edu>
2014-12-09 15:01:29 -0800

    study1

--- study1.js
+++ study1.js
@@ -1,11 +1,21 @@
 (function(){
-  var root, consentAgreed, openPretest1, out$ = typeof exports != 'undefined' && exports || this;
+  var root, consentAgreed, openPretest1, openPosttest1, installChromeExtension, startWeek1, out$ = typeof exports != 'undefined' && exports || this;
   root = typeof exports != 'undefined' && exports !== null ? exports : this;
   out$.consentAgreed = consentAgreed = function(){
-    return $('#collapseOne').collapse('hide');
+    $('#collapseOne').collapse('hide');
+    return $('#collapseTwo').collapse('show');
   };
   out$.openPretest1 = openPretest1 = function(){
-    return window.open('matching?vocab=japanese1');
+    return window.open('matching?vocab=japanese1&type=pretest');
+  };
+  out$.openPosttest1 = openPosttest1 = function(){
+    return window.open('matching?vocab=japanese1&type=posttest');
+  };
+  out$.installChromeExtension = installChromeExtension = function(){
+    return window.open('https://chrome.google.com/webstore/detail/feed-learn/ebmjdfhplinmlajmdcmhkikideknlgkf');
+  };
+  out$.startWeek1 = startWeek1 = function(){
+    return console.log('configure the extension appropriately');
   };
   $(document).ready(function(){
     return console.log('study1');
--- study1.ls
+++ study1.ls
@@ -2,9 +2,19 @@ root = exports ? this
 
 export consent-agreed = ->
   $('#collapseOne').collapse('hide')
+  $('#collapseTwo').collapse('show')
 
 export open-pretest1 = ->
-  window.open('matching?vocab=japanese1')
+  window.open('matching?vocab=japanese1&type=pretest')
+
+export open-posttest1 = ->
+  window.open('matching?vocab=japanese1&type=posttest')
+
+export install-chrome-extension = ->
+  window.open('https://chrome.google.com/webstore/detail/feed-learn/ebmjdfhplinmlajmdcmhkikideknlgkf')
+
+export start-week1 = ->
+  console.log 'configure the extension appropriately'
 
 $(document).ready ->
   console.log 'study1'
